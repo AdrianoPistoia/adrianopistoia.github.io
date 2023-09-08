f8823c2a19a2bea138a99f7dedeb657c57a42007
Adriano Pistoia <Adripistoia@gmail.com>
2023-09-08 15:43:33 -0300

    Version v 0.0.1 Push

---
+++ index.php
@@ -0,0 +1,19 @@
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./CSS/index.css">
+    <title>Portfolio Pistoia Adriano</title>
+</head>
+
+<body>
+    <div id="game">
+        <div id="player"></div>
+        
+    </div>
+    <script src="./JS/scriptInjector.js"></script>
+</body>
+
+</html>
